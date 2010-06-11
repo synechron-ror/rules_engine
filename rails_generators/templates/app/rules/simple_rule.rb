305851d76632dd5c5d49162cf66a59b6c78d6f1c
Chris Douglas <dougo.chris@gmail.com>
2010-06-11 15:52:49 +1000

    update the simple rule

--- rails_generators/templates/app/rules/simple_rule.rb
+++ rails_generators/templates/app/rules/simple_rule.rb
@@ -1,42 +1,72 @@
 class SimpleRule < RulesEngine::Rule
 
+  ##################################################################
+  # class options
   self.options = 
     {
       :group => 'Sample Rules',
-      :display_name => 'Simple Rule',
+      :display_name => 'Does Nothing',    
       :help_partial => '/re_rule_definitions/simple_rule/help',
       :new_partial => '/re_rule_definitions/simple_rule/new',
       :edit_partial => '/re_rule_definitions/simple_rule/edit'
     } 
   
-  attr_reader :title
-  
-  def attributes=(params)
-    values = params.symbolize_keys
-    @title = values[:simple_title]
+  ##################################################################
+  # set the rule data
+  def data= data
+    if data.nil?
+      @title = nil
+    else
+      @title = data
+    end  
   end
   
-  def verify
-    return "Title required" if @title.blank?
-    nil
+  ##################################################################
+  # get the rule attributes
+  def title
+    @title
+  end
+  
+  def summary
+    "Does Nothing, but called #{self.title}"
+  end
+  
+  def data
+    self.title
+  end
+  
+  def expected_outcomes
+    []
+  end
+  
+  ##################################################################
+  # set the rule attributes
+  def attributes=(params)
+    param_hash = params.symbolize_keys
+
+    @title = param_hash[:simple_title]
+  end
+  
+  ##################################################################
+  # validation and errors
+  def valid?
+    @errors = {}
+    self.errors[:simple_title] = "Title required" if self.title.blank?    
+    return self.errors.empty?
   end
 
-  # def load_from_model(re_rule)
-  #   @title = re_rule.title
-  #   true
-  # end
-  # 
-  # def save_to_model(re_rule)
-  #   it "should set the rule class" do
-  #     rule = mock('rule')
-  #     rule.should_receive(:rule_class_name=).with("MockRule")
-  #     MockRule.new.save(rule).should == true
-  #   end
-  #   re_rule.title = @title
-  #   re_rule.summary = "Simple Rule : Does Nothing"
-  #   re_rule.data = ["ignore"].to_json
-  #   re_rule.error = nil
-  #   true
-  # end    
-    
+  ##################################################################
+  # callbacks when the rule is added and removed from a pipeline
+  def after_add_to_pipeline(re_pipeline_id, re_rule_id)
+  end
+  
+  def before_remove_from_pipeline(re_pipeline_id, re_rule_id)
+  end
+  
+  ##################################################################
+  # execute the rule
+  # this rule does nothing
+  def process(job_id, data)
+    return nil
+  end  
 end
--- rails_generators/templates/spec/lib/rules/simple_rule_spec.rb
+++ rails_generators/templates/spec/lib/rules/simple_rule_spec.rb
@@ -1,4 +1,4 @@
-require File.dirname(__FILE__) + '/../spec_helper'
+require File.dirname(__FILE__) + '/../../spec_helper'
 
 describe SimpleRule do
 
@@ -8,18 +8,21 @@ describe SimpleRule do
     }
   end
   
+  def valid_data
+    'Rule Title'
+  end
+  
   it "should be discoverable" do
     RulesEngine::Discovery.rule_class("SimpleRule").should == SimpleRule
   end
 
-  describe "the expected class options" do
-    
+  describe "the expected class options" do    
     it "should be in the 'Sample Rules' group" do
       SimpleRule.options[:group].should == "Sample Rules"
     end
     
-    it "should have the diplay name of 'Simple Rule'" do
-      SimpleRule.options[:display_name].should == "Simple Rule"
+    it "should have the diplay name of 'Does Nothing'" do
+      SimpleRule.options[:display_name].should == "Does Nothing"
     end
 
     it "should have the help template of '/re_rule_definitions/simple_rule/help'" do
@@ -35,6 +38,54 @@ describe SimpleRule do
     end
   end
   
+  describe "setting the rule data" do
+    before(:each) do
+      @simple_rule = SimpleRule.new
+      @simple_rule.data = valid_data
+    end  
+    
+    describe "the data is valid" do
+      it "should be valid" do
+        @simple_rule.should be_valid
+      end
+            
+      it "should set the title" do
+        @simple_rule.title.should == "Rule Title"        
+      end
+    end
+
+    describe "the data is nil" do
+      it "should set the title to nil" do
+        @simple_rule.title.should_not be_nil
+        @simple_rule.data = nil
+        @simple_rule.title.should be_nil
+      end
+    end
+  end
+  
+  describe "the summary" do
+    it "should be include the rule title" do
+      simple_rule = SimpleRule.new
+      simple_rule.should_receive(:title).and_return("mock title")
+      simple_rule.summary.should == "Does Nothing, but called mock title"
+    end
+  end
+
+  describe "the data" do
+    it "should be the title" do
+      simple_rule = SimpleRule.new
+      simple_rule.should_receive(:title).and_return("mock title")
+      simple_rule.data.should == 'mock title'
+    end
+  end
+  
+  describe "the expected_outcomes" do
+    it "should be empty" do
+      simple_rule = SimpleRule.new
+      simple_rule.expected_outcomes.should == []
+    end
+  end
+  
   describe "setting the rule attributes" do
     before(:each) do
       @simple_rule = SimpleRule.new
@@ -42,56 +93,45 @@ describe SimpleRule do
     
     it "should be valid with valid attributes" do
       @simple_rule.attributes = valid_attributes
-      @simple_rule.verify.should be_nil
+      @simple_rule.should be_valid
     end            
-
-    it "should set the title" do
-      @simple_rule.attributes = valid_attributes
-      @simple_rule.title.should == 'Valid Simple Title'
-    end            
-    
-    it "should not be valid if the 'simple_title' attribute is missing" do
-      @simple_rule.attributes = valid_attributes.except(:simple_title)
-      @simple_rule.verify.should == "Title required"
-    end            
-    
-    it "should not be valid if the 'simple_title' attribute is blank" do
-      @simple_rule.attributes = valid_attributes.merge(:simple_title => "")
-      @simple_rule.verify.should == "Title required"
-    end                
-  end
-
-  # describe "loading a rule from an re_rule model" do
-  #   before(:each) do
-  #     @simple_rule = SimpleRule.new
-  #     @re_rule = mock_model(ReRule, :title => "Mock Model Rule Title")
-  #   end
-  #   
-  #   it "should set the title from the model" do
-  #     @simple_rule.load(@re_rule)
-  #     @simple_rule.title.should == "Mock Model Rule Title"
-  #   end        
-  # 
-  #   it "should be successfull with a successful model" do
-  #     @simple_rule.load(@re_rule).should == true
-  #   end                
-  # end
-  # 
-  # describe "saving a rule to an re_rule model" do
-  #   before(:each) do
-  #     @simple_rule = SimpleRule.new
-  #     @simple_rule.attributes = valid_attributes
-  #     @re_rule = mock_model(ReRule, :rule_class_name => true)
-  #   end
-  # 
-  #   it "should be successfull with a valid rule" do
-  #     @simple_rule.save(@re_rule).should == true
-  #   end                
-  #   
-  #   it "should save the title to the model" do
-  #     @re_rule.should_recieve(:title=).with("Valid Simple Title")
-  #     @simple_rule.save(@re_rule)
-  #   end        
-  # end
   
+    describe "setting the simple_title" do
+      it "should set the title" do
+        @simple_rule.attributes = valid_attributes
+        @simple_rule.title.should == 'Valid Title'
+      end            
+    
+      it "should not be valid if the 'simple_title' attribute is missing" do
+        @simple_rule.attributes = valid_attributes.except(:simple_title)
+        @simple_rule.should_not be_valid
+        @simple_rule.errors.should include(:simple_title)
+      end            
+    
+      it "should not be valid if the 'simple_title' attribute is blank" do
+        @simple_rule.attributes = valid_attributes.merge(:simple_title => "")
+        @simple_rule.should_not be_valid
+        @simple_rule.errors.should include(:simple_title)
+      end                
+    end
+  end
+  
+  describe "after a rule is created" do
+    # xit "There is nothing to do here"
+  end
+  
+  describe "after a rule is created" do
+    # xit "There is nothing to do here"
+  end
+  
+  describe "processing the rule" do
+    before(:each) do
+      @simple_rule = SimpleRule.new
+    end
+    
+    it "should do nothing" do
+      @simple_rule = SimpleRule.new
+      @simple_rule.process(101, {}).should be_nil
+    end        
+  end
 end
