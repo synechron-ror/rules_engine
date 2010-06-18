91f6813340fb6c0652581abe3f8bd9593a9e2374
Chris Douglas <dougo.chris@gmail.com>
2010-06-18 14:58:30 +1000

    moved images to stylesheets

--- rails_generators/templates/public/javascripts/re_job_index.js
+++ rails_generators/templates/public/javascripts/re_job_index.js
@@ -1,5 +1,23 @@
+var get_job_list = null;
+  
 $(document).ready(function() {	  
 
+  $('a.re-job-detail').live('mouseover', function(){
+      var $tr = $(this).closest('tr');    
+      if ($tr.hasClass('odd'))
+        $tr.removeClass('odd').addClass('re-job-row-odd');      
+      else
+        $tr.addClass('re-job-row');          
+    });
+  
+  $('a.re-job-detail').live('mouseout', function() {
+      var $tr = $(this).closest('tr');
+      if ($tr.hasClass('re-job-row-odd'))
+        $tr.removeClass('re-job-row-odd').addClass('odd');      
+      else
+        $tr.removeClass('re-job-row');      
+    });
+  
   $('a.re-job-detail').live('click', function() {    
     var job = $(this).attr('href').replace('#', '');
       
@@ -11,6 +29,41 @@ $(document).ready(function() {
     
   	return false;  
   });  
+
+  $('a.re-job-page-prev-enabled').live('click', function() {
+    if (!get_job_list)
+      return true
+      
+    var page = $(this).attr('href').replace('#', ''); 
+    
+    $('#re_job_list').hide();
+    $('#re_job_list_pending').show();
+    get_job_list(page);  
+    
+  	return false;  
+  });  
+
+  $('a.re-job-page-prev-disabled').live('click', function() {
+    return false;
+  });
+  
+
+  $('a.re-job-page-next-enabled').live('click', function() {
+    if (!get_job_list)
+      return true
+
+    var page = $(this).attr('href').replace('#', ''); 
+    
+    $('#re_job_list').hide();
+    $('#re_job_list_pending').show();
+    get_job_list(page);  
+    
+  	return false;  
+  });  
+
+  $('a.re-job-page-next-disabled').live('click', function() {
+    return false;
+  })
 
 });
 
