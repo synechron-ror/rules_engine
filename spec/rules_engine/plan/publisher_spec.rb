71b53a80879055666bb25abf5fa59d43d8867177
Chris Douglas <dougo.chris@gmail.com>
2010-07-12 12:09:37 +1000

    plan specs

---
+++ spec/rules_engine/plan/publisher_spec.rb
@@ -0,0 +1,49 @@
+require File.expand_path(File.dirname(__FILE__) + '/../../spec_helper')
+
+describe "RulesEngine::Plan::Publisher" do
+
+  describe "setting the publisher" do
+    it "should set the publisher as a instance of a class" do
+      mock_publisher = mock('mock_publisher')
+      RulesEngine::Plan.publisher = mock_publisher
+      RulesEngine::Plan.publisher.should == mock_publisher
+    end
+
+    it "should set the publisher to the database plan publisher" do
+      RulesEngine::Plan.publisher = :db_plan_publisher
+      RulesEngine::Plan.publisher.should be_instance_of(RulesEngine::Plan::DbPlanPublisher)
+    end
+  end
+  
+  describe "getting the publisher" do
+    it "should throw an exception if the publisher is not set" do
+      lambda {
+        RulesEngine::Plan.publisher
+      }.should raise_error
+    end        
+  end
+  
+  describe "publishing a plan" do
+    it "should throw an error if not overwritten" do
+      lambda {
+        RulesEngine::Plan::Publisher.new.publish('code', 'data')
+      }.should raise_error
+    end
+  end
+
+  describe "getting a plan" do
+    it "should throw an error if not overwritten" do
+      lambda {
+        RulesEngine::Plan::Publisher.new.get('code', '1.0.0.1')
+      }.should raise_error
+    end
+  end
+
+  describe "getting the versions" do
+    it "should throw an error if not overwritten" do
+      lambda {
+        RulesEngine::Plan::Publisher.new.versions('code')
+      }.should raise_error
+    end
+  end
+end
