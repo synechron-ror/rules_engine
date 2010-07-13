cfc194ebd0324dfb5e0b41d16a6d4108b0f83d39
Chris Douglas <dougo.chris@gmail.com>
2010-07-13 21:49:35 +1000

    template rule definitions

--- rails_generators/manifests/rules_engine_complex.rb
+++ rails_generators/manifests/rules_engine_complex.rb
@@ -19,11 +19,11 @@ class RulesEngineComplexManifest
    m.template "app/views/re_rule_definitions/complex_rule/_edit.html.erb",  "app/views/re_rule_definitions/#{rule_name}_rule/_edit.html.erb"
    m.template "app/views/re_rule_definitions/complex_rule/_help.html.erb",  "app/views/re_rule_definitions/#{rule_name}_rule/_help.html.erb"
    m.template "app/views/re_rule_definitions/complex_rule/_new.html.erb",  "app/views/re_rule_definitions/#{rule_name}_rule/_new.html.erb"
-   m.template "app/views/re_rule_definitions/complex_rule/_pipeline.html.erb",  "app/views/re_rule_definitions/#{rule_name}_rule/_pipeline.html.erb"
    m.template "app/views/re_rule_definitions/complex_rule/_script.html.erb",  "app/views/re_rule_definitions/#{rule_name}_rule/_script.html.erb"
    m.template "app/views/re_rule_definitions/complex_rule/_title.html.erb",  "app/views/re_rule_definitions/#{rule_name}_rule/_title.html.erb"
    m.template "app/views/re_rule_definitions/complex_rule/_word.html.erb",  "app/views/re_rule_definitions/#{rule_name}_rule/_word.html.erb"
    m.template "app/views/re_rule_definitions/complex_rule/_words.html.erb",  "app/views/re_rule_definitions/#{rule_name}_rule/_words.html.erb"
+   m.template "app/views/re_rule_definitions/complex_rule/_workflow.html.erb",  "app/views/re_rule_definitions/#{rule_name}_rule/_workflow.html.erb"
    m.template "spec/lib/rules/complex_rule_spec.rb",  "spec/lib/rules/#{rule_name}_rule_spec.rb"
 
   end
