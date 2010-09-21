9cdbbcc4ca4252c525e2137d6b25c5ea1274ba22
Chris Douglas <dougo.chris@gmail.com>
2010-09-22 08:43:44 +1000

    pre gemerator

--- spec/rails_3_0_0_root/config/routes.rb
+++ spec/rails_3_0_0_root/config/routes.rb
@@ -48,7 +48,7 @@ Rails300Root::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => "welcome#index"
+  root :to => "re_plans#template"
 
   # See how all your routes lay out with "rake routes"
 
