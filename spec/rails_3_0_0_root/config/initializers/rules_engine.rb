8367e87a27473865ea180fd3f79019a413325a39
Chris Douglas <dougo.chris@gmail.com>
2010-09-23 16:30:49 +1000

    not required

--- spec/rails_3_0_0_root/config/initializers/rules_engine.rb
+++
@@ -1,8 +0,0 @@
-RulesEngine::Discovery.discover!
-
-RulesEngine::Publish.publisher = :db_publisher
-RulesEngine::Process.runner = :db_runner
-RulesEngine::Process.auditor = :db_auditor
-RulesEngine::Process.auditor.audit_level = RulesEngine::Process::AUDIT_INFO
-
-RulesEngineView::Defer.prefix_breadcrumbs = nil
