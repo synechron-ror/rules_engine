348b24f9c6d822d58dffb75f93a8a17ae1e81ef8
Chris Douglas <dougo.chris@gmail.com>
2010-09-27 14:52:27 +1000

    cache options

--- generators/manifests/templates/config/initializers/rules_engine.rb
+++ generators/manifests/templates/config/initializers/rules_engine.rb
@@ -5,5 +5,13 @@ RulesEngine::Process.runner = :db_runner
 RulesEngine::Process.auditor = :db_auditor
 RulesEngine::Process.auditor.audit_level = RulesEngine::Process::AUDIT_INFO
 
+RulesEngine::Cache.cache_store = nil
+# RulesEngine::Cache.cache_store = :mem_cache_store, "localhost:11210"
+# RulesEngine::Cache.cache_store = :memory_store
+# RulesEngine::Cache.cache_store = :file_store, "/path/to/cache/directory"
+# RulesEngine::Cache.cache_store = :drb_store, "druby://localhost:9192"
+# RulesEngine::Cache.cache_store = :mem_cache_store, "localhost"
+# RulesEngine::Cache.cache_store = MyOwnStore.new("parameter")
+
 RulesEngineView::Config.layout = nil
 RulesEngineView::Config.prefix_breadcrumbs = nil
